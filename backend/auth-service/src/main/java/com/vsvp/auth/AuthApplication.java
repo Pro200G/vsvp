bd037389b5121794c73a876ebc034a41c1063008
Pro200G <78156285+Pro200G@users.noreply.github.com>
2026-02-25 20:48:30 +0300

    Scaffold multi-service platform structure for three-role app

---
+++ backend/auth-service/src/main/java/com/vsvp/auth/AuthApplication.java
@@ -0,0 +1,7 @@
+package com.vsvp.auth;
+
+public class AuthApplication {
+    public static void main(String[] args) {
+        // TODO: SpringBootApplication entrypoint
+    }
+}
---
+++ backend/map-service/src/main/java/com/vsvp/map/MapApplication.java
@@ -0,0 +1,7 @@
+package com.vsvp.map;
+
+public class MapApplication {
+    public static void main(String[] args) {
+        // TODO: SpringBootApplication entrypoint
+    }
+}
